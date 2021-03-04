675b2250e80d59891db6f0df67eea192647bbd36
Kaishi <nkaishi927@gmail.com>
2021-03-05 02:25:58 +0900

    update

--- ios/Classes/SwiftFacebookDeeplinksPlugin.swift
+++ ios/Classes/SwiftFacebookDeeplinksPlugin.swift
@@ -6,11 +6,10 @@ let MESSAGES_CHANNEL = "ru.proteye/facebook_deeplinks/channel"
 let EVENTS_CHANNEL = "ru.proteye/facebook_deeplinks/events"
 
 public class SwiftFacebookDeeplinksPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
-  var _eventSink: FlutterEventSink?
-  var _initialUrl: String = ""
-  // links will be added to this queue until the sink is ready to process them
-  var _queuedLinks = [String]()
-  
+
+  private var latestLink: String?
+  private var eventSink: FlutterEventSink?
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let instance = SwiftFacebookDeeplinksPlugin()
 
@@ -23,21 +22,23 @@ public class SwiftFacebookDeeplinksPlugin: NSObject, FlutterPlugin, FlutterStrea
     registrar.addApplicationDelegate(instance)
   }
 
-  // Doesn't work!!!
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     if let url = launchOptions?[.url] as? URL {
-      _initialUrl = url.absoluteString
-      self.handleLink(url.absoluteString)
+      let link = url.absoluteString
+      handleLink(link)
     }
 
     Settings.isAutoInitEnabled = true
-    ApplicationDelegate.initializeSDK(nil)
+    ApplicationDelegate.initializeSDK(launchOptions)
+    ApplicationDelegate.shared.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
     AppLinkUtility.fetchDeferredAppLink { (url, error) in
       if let error = error {
         print("Received error while fetching deferred app link %@", error)
       }
       if let url = url {
-        self.handleLink(url.absoluteString)
         if #available(iOS 10, *) {
           UIApplication.shared.open(url, options: [:], completionHandler: nil)
         } else {
@@ -45,39 +46,53 @@ public class SwiftFacebookDeeplinksPlugin: NSObject, FlutterPlugin, FlutterStrea
         }
       }
     }
-    return true
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    return self.handleLink(url.absoluteString)
+    ApplicationDelegate.shared.application(
+      app,
+      open: url,
+      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+    )
+    handleLink(url.absoluteString)
+    return super.application(app, open: url, options: options)
+  }
+
+  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+      let link = userActivity.webpageURL?.absoluteString
+      if let link = link {
+        handleLink(link)
+      }
+    }
+    return super.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if call.method == "initialUrl" {
-      result(_initialUrl)
+      result(latestLink)
     } else {
       result(FlutterMethodNotImplemented)
     }
   }
 
   public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
-    _eventSink = eventSink
-    _queuedLinks.forEach({ eventSink($0) })
-    _queuedLinks.removeAll()
+    self.eventSink = eventSink
     return nil
   }
 
   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    _eventSink = nil
+    eventSink = nil
     return nil
   }
 
-  private func handleLink(_ link: String) -> Bool {
-    guard let eventSink = _eventSink else {
-      _queuedLinks.append(link)
-      return false
+  private func handleLink(_ link: String) {
+    latestLink = link
+    guard let eventSink = eventSink else {
+      return
     }
     eventSink(link)
-    return true
   }
 }
