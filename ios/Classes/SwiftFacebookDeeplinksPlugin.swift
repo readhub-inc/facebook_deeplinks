e4cfbdffda6612319833b3f8e0ba0a7af3fd1a50
Kaishi <nkaishi927@gmail.com>
2021-03-05 04:05:12 +0900

    update

--- ios/Classes/SwiftFacebookDeeplinksPlugin.swift
+++ ios/Classes/SwiftFacebookDeeplinksPlugin.swift
@@ -6,93 +6,70 @@ let MESSAGES_CHANNEL = "ru.proteye/facebook_deeplinks/channel"
 let EVENTS_CHANNEL = "ru.proteye/facebook_deeplinks/events"
 
 public class SwiftFacebookDeeplinksPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+    private var latestLink: String?
+    private var eventSink: FlutterEventSink?
 
-  private var latestLink: String?
-  private var eventSink: FlutterEventSink?
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftFacebookDeeplinksPlugin()
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let instance = SwiftFacebookDeeplinksPlugin()
+        let channel = FlutterMethodChannel(name: MESSAGES_CHANNEL, binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(instance, channel: channel)
 
-    let channel = FlutterMethodChannel(name: MESSAGES_CHANNEL, binaryMessenger: registrar.messenger())
-    registrar.addMethodCallDelegate(instance, channel: channel)
+        let streamChannel = FlutterEventChannel(name: EVENTS_CHANNEL, binaryMessenger: registrar.messenger())
+        streamChannel.setStreamHandler(instance)
 
-    let streamChannel = FlutterEventChannel(name: EVENTS_CHANNEL, binaryMessenger: registrar.messenger())
-    streamChannel.setStreamHandler(instance)
-
-    registrar.addApplicationDelegate(instance)
-  }
-
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    if let url = launchOptions?[.url] as? URL {
-      let link = url.absoluteString
-      handleLink(link)
+        registrar.addApplicationDelegate(instance)
     }
 
-    Settings.isAutoInitEnabled = true
-    ApplicationDelegate.initializeSDK(launchOptions)
-    ApplicationDelegate.shared.application(
-      application,
-      didFinishLaunchingWithOptions: launchOptions
-    )
-    AppLinkUtility.fetchDeferredAppLink { (url, error) in
-      if let error = error {
-        print("Received error while fetching deferred app link %@", error)
-      }
-      if let url = url {
-        if #available(iOS 10, *) {
-          UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        } else {
-          UIApplication.shared.openURL(url)
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+        if let url = launchOptions[UIApplication.LaunchOptionsKey.url] as? URL {
+            _ = self.handleLink(url.absoluteString)
         }
-      }
+        ApplicationDelegate.initializeSDK(launchOptions as? [UIApplication.LaunchOptionsKey: Any])
+        AppLinkUtility.fetchDeferredAppLink { (url, error) in
+            if let error = error {
+                print("Received error while fetching deferred app link %@", error)
+            }
+            if let url = url {
+                _ = self.handleLink(url.absoluteString)
+                if #available(iOS 10, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+        return true
     }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 
-  public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    ApplicationDelegate.shared.application(
-      app,
-      open: url,
-      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-    )
-    handleLink(url.absoluteString)
-    return super.application(app, open: url, options: options)
-  }
-
-  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
-      let link = userActivity.webpageURL?.absoluteString
-      if let link = link {
-        handleLink(link)
-      }
+    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return self.handleLink(url.absoluteString)
     }
-    return super.application(application, continue: userActivity, restorationHandler: restorationHandler)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method == "initialUrl" {
-      result(latestLink)
-    } else {
-      result(FlutterMethodNotImplemented)
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "initialUrl" {
+            result(latestLink)
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
     }
-  }
 
-  public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
-    self.eventSink = eventSink
-    return nil
-  }
-
-  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    eventSink = nil
-    return nil
-  }
-
-  private func handleLink(_ link: String) {
-    latestLink = link
-    guard let eventSink = eventSink else {
-      return
+    public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = eventSink
+        return nil
     }
-    eventSink(link)
-  }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        eventSink = nil
+        return nil
+    }
+
+    private func handleLink(_ link: String) -> Bool {
+        latestLink = link
+        guard let eventSink = eventSink else {
+            return false
+        }
+        eventSink(link)
+        return true
+    }
 }
